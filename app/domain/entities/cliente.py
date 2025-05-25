825f54abd591ffe1214edb2897114ec5724bca59
ivan <ivanrisso@gmail.com>
2025-05-25 20:38:35 -0300

    actualizo

--- app/domain/entities/cliente.py
+++ app/domain/entities/cliente.py
@@ -1,14 +1,14 @@
 from typing import Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 
 class Cliente(BaseModel):
-    id: Optional[int]
+    id: Optional[int] = None
     nombre: Optional[str]
     apellido: Optional[str]
     razon_social: Optional[str]
     cuit: Optional[str]
-    email: Optional[str]
+    email: EmailStr
     tipo_doc_id: Optional[int]
     iva_id: Optional[int]
     
